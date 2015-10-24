92db3c5c0c19c833529745a39a01cbad2b217ed5
syyan <syyan@thoughtworks.com>
2015-10-24 19:16:00 +0800

    delete world

--- src/features/support/world.js
+++
@@ -1,8 +0,0 @@
-var World, chai, chaiAsPromised;
-chai = require('chai');
-chaiAsPromised = require('chai-as-promised');
-module.exports.World = function World(callback) {
-    chai.use(chaiAsPromised);
-    this.expect = chai.expect;
-    callback();
-};
